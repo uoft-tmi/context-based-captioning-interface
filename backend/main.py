ec42c3b8ea806351650d6fa9022b336a00545e83
Peter Lee <psjlee42@gmail.com>
2026-04-01 21:39:30 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,6 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
